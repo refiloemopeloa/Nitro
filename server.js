03c48843adf9b01d1f23cbe5afd35abe998d49ff
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-10-20 14:00:46 +0200

    Partial API

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const leaderBoard = require('./src/leaderBoard.json');
+
+
+app.use(bodyParser.json());
+
+
+app.get('/api/leaderBoard', (req, res) => {
+   
+    res.json({ message: leaderBoard });
+  });
+
+  app.post('/api/leaderBoard', (req, res) => {
+    const newName = req.body.name;
+    const newScore = req.body.score;
+    //todos.push(newTodo);
+    //res.status(201).json(newTodo);
+    res.json({ message: newName });
+  });
+  
+const port = process.env.PORT || 5173;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
