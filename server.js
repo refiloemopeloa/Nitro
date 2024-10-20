d5eab35a2064929c0e8a5c82157087609fc3f944
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-10-20 16:39:46 +0200

    Add new player

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const leaderBoard = require('./src/leaderBoard.json');
+const fs = require('fs'); 
+const filePath = './src/leaderBoard.json';
+const rawData = fs.readFileSync(filePath);
+const jsonData = JSON.parse(rawData); 
+
 
 
 app.use(bodyParser.json());
@@ -10,18 +15,22 @@ app.use(bodyParser.json());
 
 app.get('/api/leaderBoard', (req, res) => {
    
-    res.json({ message: leaderBoard });
+    res.json({ message: jsonData});
   });
 
   app.post('/api/leaderBoard', (req, res) => {
     const newName = req.body.name;
     const newScore = req.body.score;
-    //todos.push(newTodo);
+    //jsonData.name = newName;
+    //jsonData.score = newScore;
+    jsonData.push({"name": newName, "score": newScore});
+    fs.writeFileSync(filePath, JSON.stringify(jsonData));
+    //leaderBoard.push({"name": newName, "score": newScore});
     //res.status(201).json(newTodo);
-    res.json({ message: newName });
+    res.json({ message: jsonData });
   });
   
-const port = process.env.PORT || 5173;
+const port = process.env.PORT || 5174;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
