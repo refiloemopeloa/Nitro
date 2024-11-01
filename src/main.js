fb18410bfba840d1be23f1a5bb476f74adbef6a6
SeanMz28 <2438634@students.wits.ac.za>
2024-11-01 17:15:17 +0200

    Removed debug tools from HUD on Level 1

--- src/main.js
+++ src/main.js
@@ -160,13 +160,13 @@ if (WebGL.isWebGL2Available()) {
     const menuButton = document.getElementById('menuButton');
     const modal = document.getElementById('modal');
     const modalOverlay = document.getElementById('modalOverlay');
-    const settingsButton = document.getElementById('settingsButton');
+    //const settingsButton = document.getElementById('settingsButton');
     const quitButton = document.getElementById('quitButton');
     const resumeButton = document.getElementById('resumeButton');
 
     menuButton.addEventListener('click', openModal);
     modalOverlay.addEventListener('click', closeModal);
-    settingsButton.addEventListener('click', openSettings);
+    //settingsButton.addEventListener('click', openSettings);
     quitButton.addEventListener('click', quitGame);
     resumeButton.addEventListener('click', closeModal);
 
@@ -174,14 +174,14 @@ if (WebGL.isWebGL2Available()) {
         modal.style.display = 'block';
         modalOverlay.style.display = 'block';
         isPaused = true;
-        controls.disable(); // Disable car controls
+        //controls.disable(); // Disable car controls
     }
 
     function closeModal() {
         modal.style.display = 'none';
         modalOverlay.style.display = 'none';
         isPaused = false;
-        controls.enable(); // Re-enable car controls
+        //controls.enable(); // Re-enable car controls
     }
 
     function openSettings() {
@@ -1328,10 +1328,10 @@ document.addEventListener('keydown', (event) => {
                 restoreCarAppearance();
             }
 
-            const position = vehicle.chassisBody.position;
-            document.getElementById('debug-pos-x').textContent = position.x.toFixed(2);
-            document.getElementById('debug-pos-y').textContent = position.y.toFixed(2);
-            document.getElementById('debug-pos-z').textContent = position.z.toFixed(2);
+            // const position = vehicle.chassisBody.position;
+            // document.getElementById('debug-pos-x').textContent = position.x.toFixed(2);
+            // document.getElementById('debug-pos-y').textContent = position.y.toFixed(2);
+            // document.getElementById('debug-pos-z').textContent = position.z.toFixed(2);
 
         }
 
