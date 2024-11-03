0fbeb8d5a5f6c1380f9aabf317eacf82855a6ef2
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-11-03 20:31:04 +0200

    fixed webgl import

--- src/level1.js
+++ src/level1.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import * as CANNON from 'cannon-es';
-import WebGL from 'three/addons/capabilities/WebGL.js';
+import WebGL from 'three/WebGL.js';
 import { OrbitControls } from 'three/controls/OrbitControls.js';
 import { CameraManager } from './camera.js';
 import { Controls } from './controls.js';
